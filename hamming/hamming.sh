8fa896706488b64b919fb0ea40de4a0ab0db269f
Iain Diamond <iain.diamond@gmail.com>
2019-02-12 12:46:34 +0000

    Check for unset variables

--- hamming/hamming.sh
+++ hamming/hamming.sh
@@ -1,26 +1,30 @@
 #!/usr/bin/env bash
 
+# Why does errexit break this code but not other code examples?
+# set -o errexit
+set -o nounset
+
 function main() {
   if [ $# -ne 2 ]; then
     echo "Usage: hamming.sh <string1> <string2>"
     exit 1
   fi
-  strand1=$1
-  strand2=$2
+  local strand1=$1
+  local strand2=$2
   if [ "${#strand1}" -ne "${#strand2}" ]; then
     echo "left and right strands must be of equal length"
     exit 1
   fi
   
-  counter=0
+  local counter=0
   
   for (( i=0 ; i < ${#strand1} ; i++ )); do
     if [ "${strand1:i:1}" != "${strand2:i:1}" ]; then
-      (( counter ++ ))
+      ((counter++))
     fi
   done
   
-  echo $counter
+  echo "$counter"
 }
 
 main "$@"
