ac6fe0bd12bbbe6079a17a661c826347256d9f5e
Iain Diamond <iain.diamond@gmail.com>
2019-02-15 23:09:34 +0000

    refactor case statement

--- scrabble-score/scrabble_score.sh
+++ scrabble-score/scrabble_score.sh
@@ -4,22 +4,14 @@ function scoreLetter() {
   local letter=$1
   
   case ${letter,,} in
-    a|e|i|o|u|l|n|r|s|t)
-    echo 1;;
-    d|g)
-    echo 2;;
-    b|c|m|p)
-    echo 3;;
-    f|h|v|w|y)
-    echo 4;;
-    k)
-    echo 5;;
-    j|x)
-    echo 8;;
-    q|z)
-    echo 10;;
-    *)
-    echo 0;;
+    a|e|i|o|u|l|n|r|s|t)  echo 1;;
+    d|g)                  echo 2;;
+    b|c|m|p)              echo 3;;
+    f|h|v|w|y)            echo 4;;
+    k)                    echo 5;;
+    j|x)                  echo 8;;
+    q|z)                  echo 10;;
+    *)                    echo 0;;
   esac
   
   
