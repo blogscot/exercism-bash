f5088dbf7712f2a4ca268a6c7de4bdd15b1e69ac
Iain Diamond <iain.diamond@gmail.com>
2019-02-16 14:27:23 +0000

    Add atbash cipher solution

---
+++ atbash_cipher/atbash_cipher.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+function encode() {
+  echo $(echo ${1,,} | tr -d "[:space:][:punct:]" |  tr [a-z] [zyxwvutsrqponmlkjihgfedcba] | fold -w 5)
+}
+
+function decode() {
+  echo $1 | tr -d "[:space:]" | tr [zyxwvutsrqponmlkjihgfedcba] [a-z]
+}
+
+"$@"
